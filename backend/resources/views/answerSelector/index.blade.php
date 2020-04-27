3a76f59bbd78e45cb2ae0152afc11d38c58c1b3d
somogyitomi <32802865+somogyitomi@users.noreply.github.com>
2020-04-27 21:09:31 +0200

    index.php v0.01
    index béta versio

--- backend/resources/views/answerSelector/index.blade.php
+++ backend/resources/views/answerSelector/index.blade.php
@@ -1,4 +1,67 @@
 @extends('layouts.app')
 @section('content')
 {{-- Ide jöhet a html + php content --}}
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Belépés</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style type="text/css">
+   .box{
+    width:600px;
+    margin:0 auto;
+    border:1px solid #ccc;
+   }
+  </style>
+ </head>
+ <body>
+  <br />
+  <div class="container box">
+   <h3 align="center">Általános Iskolai Történelmi Kvíz Belépés</h3><br />
+
+   @if(isset(Auth::user()->email))
+    <script>window.location="/main/successlogin";</script>
+   @endif
+
+   @if ($message = Session::get('error'))
+   <div class="alert alert-danger alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+   </div>
+   @endif
+
+   @if (count($errors) > 0)
+    <div class="alert alert-danger">
+     <ul>
+     @foreach($errors->all() as $error)
+      <li>{{ $error }}</li>
+     @endforeach
+     </ul>
+    </div>
+   @endif
+
+   <form method="post" action="{{ url('/main/checklogin') }}">
+    {{ csrf_field() }}
+    <div class="form-group">
+     <label>Név</label>
+     <input type="email" name="email" class="form-control" />
+    </div>
+    <div class="form-group">
+     <label>Jelszó</label>
+     <input type="password" name="password" class="form-control" />
+    </div>
+    <div class="form-group">
+     <input type="submit" name="login" class="btn btn-primary" value="Bejelentkezés" />
+    </div>
+   </form>
+  </div>
+ </body>
+</html>
+
+
+
 @endsection
